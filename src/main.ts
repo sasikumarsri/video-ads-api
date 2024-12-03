c6cbbb8bddd3220436c33592585f3416035d8233
sasikumarsri <sasikumar.dharmaraj@gmail.com>
2024-12-04 00:55:49 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // Start the application
-  await app.listen(3000, '0.0.0.0');
-  console.log(`Application is running on: http://localhost:3000`);
-  console.log(`Swagger docs available at: http://localhost:3000/api`);
+  const port = process.env.PORT || 3000
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger docs available at: http://localhost:${port}/api`);
 }
 bootstrap();
