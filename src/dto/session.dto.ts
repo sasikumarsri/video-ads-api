34a86f91d8fc05b951c9214e4a052e4da04e7d25
sk <37347003+sasikumarsri@users.noreply.github.com>
2024-12-04 02:44:24 +0530

    Update session.dto.ts

--- src/dto/session.dto.ts
+++ src/dto/session.dto.ts
@@ -1,15 +1,10 @@
-import { IsString, IsDate, IsNotEmpty } from 'class-validator';
 
 export class SessionResponseDto {
-  @IsString()
   session_token: string;
 
-  @IsDate()
   expires_at: Date;
 }
 
 export class CreateSessionDto {
-  @IsString()
-  @IsNotEmpty()
   user_id: number;
 }
